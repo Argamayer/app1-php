143a8ca8903d1188734eebf1ebc5254b876722b9
Alief Febri Indryanto <160709033+Argamayer@users.noreply.github.com>
2025-10-17 13:14:17 +0700

    mengubah index test CI/CD
    Test CI/CD

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo '<h1>Selamat datang di Application 1</h1>';
 echo '<p>Ini dijalankan pada port 2000</p>';
 echo '<p>Server Time: ' . date('Y-m-d H:i:s') . '</p>';
-phpinfo();
+echo '<h1>Diupdate Melalui CI/CD!<h1>';
 ?>
